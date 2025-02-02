8b359056d1bdff9729f040e8df6575f5f436bd3d
Prabal-saxena <prabalsaxena98@gmail.com>
2025-02-01 22:54:11 -0500

    Sample Postgres SQL instance creation

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "google_sql_database_instance" "pgsql-instance" {
+  name             = "pg-instance"
+  project          = "spheric-base-448422-q9"
+  region           = "us-central1"
+  database_version = "POSTGRES_15"
+
+  settings {
+    tier = "db-custom-2-7680"
+    password_validation_policy {
+      min_length                  = 6
+      reuse_interval              = 2
+      complexity                  = "COMPLEXITY_DEFAULT"
+      disallow_username_substring = true
+      password_change_interval    = "30s"
+      enable_password_policy      = true
+    }
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
